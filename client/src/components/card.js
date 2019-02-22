1c1f21e003df1de367379d7a80c1cf40d7aaa5a1
angrylobster <liewminshan@gmail.com>
2019-02-22 16:14:50 +0800

    [F][Cards]Upvote and downvote toggle

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,43 +1,94 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
-  render() {
-    return (
-        <div className="d-flex p-2">
-            <div
-                className="div__wrapper-votearrows"
-            >
-                <img 
-                    src="upvote.png"
-                    className="w-100"
-                    // className="img__votearrows"
-                />
-                <img 
-                    src="downvote.png"
-                    className="w-100"
 
-                    // className="img__votearrows"
-                />
+    constructor(){
+        super();
+        this.toggleVote = this.toggleVote.bind(this);
+        this.state={
+            upvoted: false,
+            downvoted: false
+        }
+    }
 
-            </div> 
-            <div
-                className="w-100 pl-2"
-            >
+    toggleVote(arrowClicked){
+        if (arrowClicked === 'down'){
+            if (this.state.downvoted){
+                this.setState({
+                    upvoted: false,
+                    downvoted: false
+                })
+            } else {
+                this.setState({
+                    upvoted: false,
+                    downvoted: true
+                })
+            }
+        } else {
+            if (this.state.upvoted){
+                this.setState({
+                    upvoted: false,
+                    downvoted: false
+                })
+            } else {
+                this.setState({
+                    upvoted: true,
+                    downvoted: false
+                })
+            }
+        }
+    }
+
+    getUpvoteArrow(){
+        if (this.state.upvoted){
+            return (
+                <img src="upvoted.png" onClick={ () => { this.toggleVote('up') }} alt="Upvoted arrow"/>
+            )
+        } else {
+            return (
+                <img src="upvote.png" onClick={ () => { this.toggleVote('up') }} alt="Upvote arrow"/>
+            )
+        }
+    }
+
+    getDownvoteArrow(){
+        if (this.state.downvoted){
+            return (
+                <img src="downvoted.png" onClick={ () => { this.toggleVote('down') }} alt="Downvoted arrow"/>
+            )
+        } else {
+            return (
+                <img src="downvote.png" onClick={ () => { this.toggleVote('down') }} alt="Downvote arrow"/>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div className="d-flex p-2">
                 <div
-                    className="d-flex"
+                    className="div__wrapper-votearrows"
                 >
-                    <small>Username</small>
-                    <small>1000 points</small>
-                    <small>59 mins ago</small>
-                </div>
-                <div>
-                    <p>This is a caption about something people said some time ago but nobody knows what they meant so some people are asking about it.</p>
+                    { this.getUpvoteArrow() }
+                    { this.getDownvoteArrow() }
+                </div> 
+                <div
+                    className="w-100 pl-2"
+                >
+                    <div
+                        className="d-flex"
+                    >
+                        <small>Username</small>
+                        <small>1000 points</small>
+                        <small>59 mins ago</small>
+                    </div>
+                    <div>
+                        <p>This is a caption about something people said some time ago but nobody knows what they meant so some people are asking about it.</p>
+                    </div>
                 </div>
             </div>
-
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default Card;
