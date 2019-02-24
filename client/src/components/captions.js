2d6ac55d1be792daf95f037023c8bde6c27a496f
angrylobster <liewminshan@gmail.com>
2019-02-24 14:46:14 +0800

    [F][New Caption]Post captions using enter key

--- client/src/components/captions.js
+++ client/src/components/captions.js
@@ -35,22 +35,23 @@ class NewCaption extends Component{
         });
     }
 
-    postCaption() {
-        fetch('/captions.json', {
-            method: 'POST',
-            header: {
-                'Accept': 'application/json',
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify({body: 'What the hell?', user_id: 1})
+    postCaption(input) {
+        alert(input)
+        // fetch('/captions.json', {
+        //     method: 'POST',
+        //     header: {
+        //         'Accept': 'application/json',
+        //         'content-type': 'application/json'
+        //     },
+        //     body: JSON.stringify({body: 'What the hell?', user_id: 1})
             
-        }).then(response => {
-            if (response.ok) { return response.json() } throw new Error('Request failed!');
-        }, networkError => {
-            console.log(networkError.message);
-        }).then(jsonResponse => {
-            return jsonResponse;
-        });
+        // }).then(response => {
+        //     if (response.ok) { return response.json() } throw new Error('Request failed!');
+        // }, networkError => {
+        //     console.log(networkError.message);
+        // }).then(jsonResponse => {
+        //     return jsonResponse;
+        // });
     }
 
     render(){
@@ -61,8 +62,12 @@ class NewCaption extends Component{
                 <input 
                     className="border-0 mt-3 w-100 pl-2 pr-2"     
                     placeholder="Write your caption here..."
+                    onKeyDown={e => { 
+                        if (e.keyCode === 13){
+                            this.postCaption(e.target.value);
+                        } 
+                    }}
                 />
-                <button onClick={this.signUp}>Post</button>
             </div>
         )
 
